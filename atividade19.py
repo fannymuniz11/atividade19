65d7b7bfa21f7d556ad2c05e7044cda7f023ac6c
fannymuniz11 <munizfanny03@gmail.com>
2024-09-18 15:36:38 -0300

    atividade19

---
+++ atividade19.py
@@ -0,0 +1,10 @@
+# Crie um programa que receba um número inteiro e calcule o fatorial desse número usando uma estrutura de repetição.
+resultado= 1
+N = int(input("Seu numero"))
+F=1
+while N > 1:
+    F*=N
+    N- =1
+    print(F"Seu fatorial é {F}")
+    
+
